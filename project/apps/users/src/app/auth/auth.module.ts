4936e4a463177de88d5d165a0c8b8aba768b9897
bibitor1 <evgeniishogolev93@gmail.com>
2023-12-08 20:03:44 +0300

    К проверке дз

--- project/apps/users/src/app/auth/auth.module.ts
+++ project/apps/users/src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
