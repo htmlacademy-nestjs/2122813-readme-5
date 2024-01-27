470065d33d51f6216483993b906fceb7510653b3
bibitor1 <evgeniishogolev93@gmail.com>
2024-01-27 11:58:18 +0300

    Рефакторинг модуля blog

--- project/libs/models/blog-models/prisma/seed.ts
+++ project/libs/models/blog-models/prisma/seed.ts
@@ -1,105 +1,72 @@
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
 async function fillDb() {
-  await prisma.type.upsert({
-    where: { typeId: 1 },
+  await prisma.post.upsert({
+    where: { postId: 1 },
     update: {},
     create: {
+      type: 'video',
       title: 'Видео',
-      posts: {
-        create: [
-          {
-            title: 'Видео',
-            link: 'https://www.youtube.com/watch?v=piqdKWNnqBo',
-            userId: '1'
-          },
-        ]
-      },
+      link: 'https://www.youtube.com/watch?v=piqdKWNnqBo',
+      userId: '1'
     }
   });
 
-  await prisma.type.upsert({
-    where: { typeId: 2 },
+  await prisma.post.upsert({
+    where: { postId: 2 },
     update: {},
     create: {
+      type: 'text',
       title: 'Текст',
-      posts: {
+      announce: 'Анонс публикации',
+      description: 'Текст публикации',
+      userId: '2',
+      comments: {
         create: [
           {
-            title: 'Текст',
-            announce: 'Анонс публикации',
-            description: 'Текст публикации',
+            message: 'Вау!',
             userId: '2',
-            comments: {
-              create: [
-                {
-                  message: 'Вау!',
-                  userId: '2',
-                }
-              ]
-            }
-          },
+          }
         ]
-      },
+      }
     }
   });
 
-  await prisma.type.upsert({
-    where: { typeId: 3 },
+  await prisma.post.upsert({
+    where: { postId: 3 },
     update: {},
     create: {
-      title: 'Цитата',
-      posts: {
-        create: [
-          {
-            author: 'Автор цитаты',
-            description: 'Цитата',
-            userId: '3'
-          },
-        ]
-      },
+      type: 'quote',
+      author: 'Автор цитаты',
+      description: 'Цитата',
+      userId: '3'
     }
   });
 
-  await prisma.type.upsert({
-    where: { typeId: 4 },
+  await prisma.post.upsert({
+    where: { postId: 4 },
     update: {},
     create: {
-      title: 'Фото',
-      posts: {
-        create: [
-          {
-            photo: 'https://loremflickr.com/640/360',
-            userId: '4'
-          },
-        ]
-      },
+      type: 'photo',
+      photo: 'https://loremflickr.com/640/360',
+      userId: '4'
     }
   });
 
-  await prisma.type.upsert({
-    where: { typeId: 5 },
+  await prisma.post.upsert({
+    where: { postId: 5 },
     update: {},
     create: {
-      title: 'Ссылка',
-      posts: {
-        create: [
-          {
-            link: 'https://www.youtube.com/watch?v=piqdKWNnqBo',
-            description: 'Описание ссылки',
-            userId: '5'
-          },
-        ]
-      },
+      type: 'link',
+      link: 'https://www.youtube.com/watch?v=piqdKWNnqBo',
+      description: 'Описание ссылки',
+      userId: '5'
     }
   });
 
   console.info('🤘️ Database was filled')
 }
-
-
 fillDb()
   .then(async () => {
     await prisma.$disconnect()
@@ -107,4 +74,5 @@ fillDb()
   .catch(async (err) => {
     console.error(err);
     await prisma.$disconnect()
+    // process.exit(1);
   })
