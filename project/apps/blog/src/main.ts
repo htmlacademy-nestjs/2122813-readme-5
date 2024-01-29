145944bb17f5966a384a1ec9683fd93a2582f6d3
bibitor1 <evgeniishogolev93@gmail.com>
2024-01-29 14:04:54 +0300

    Доработали лайки/комментарии/API

--- project/apps/blog/src/main.ts
+++ project/apps/blog/src/main.ts
@@ -2,9 +2,17 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Blog» service')
+    .setDescription('Blog service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
@@ -12,11 +20,13 @@ async function bootstrap() {
     transform: true,
   }));
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3005;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
-
 bootstrap();
